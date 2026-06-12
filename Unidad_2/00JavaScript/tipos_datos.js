d04563be820ff0b31906d0b0315fe279ac52ca23
bluebul <mrodrgiuezsaraguro@gmail.com>
2026-06-12 13:07:01 -0500

    Feat:Aprendiendo como imprimit en consola y los tipos de datos

---
+++ Unidad_2/00JavaScript/tipos_datos.js
@@ -0,0 +1,36 @@
+//string
+let_nombre = 'Maria';
+
+//NUMERICO
+let edad = 25;
+
+//BOOLEANS
+let esMayorDeEdad = true;
+
+//null
+let noHayValor = null;
+
+//nodefined
+let noDefinida = undefined;
+
+//symbol valor unico
+let simboloUnico = Sumbol('único');
+
+//datos gigantes bigint
+let numeroGrande = 2n;
+
+//COMPLETOS --- Parte de objetos
+//object
+let carro = {
+    marca : 'Tesla',
+    modelo: 'Model S'
+}
+
+//arrays
+let frutas =['manzana', 'pera', 'piña'];
+
+//funtions
+function saludar (){}
+
+
+
