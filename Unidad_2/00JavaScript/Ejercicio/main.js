8c494b9c946a46a920309080c82ec20a38c8897d
mariajose3440 <mjrodriguez@unl.edu.ec>
2026-06-17 10:25:14 -0500

    Homework: consulta de los condicionales y el html (17 de junio del 2026)

---
+++ Unidad_2/00JavaScript/Ejercicio/main.js
@@ -0,0 +1,8 @@
+alert("Mi primera pagina :D"); 
+let string2 = 'No camines delante de mí, puede que no te siga. No camines detrás de mí, puede que no te guíe. Camina junto a mí y sé mi amigo.';
+
+/**
+ * busca el elemento HTML con el id "frase" y reemplaza su contenido de texto por el valor almacenado en la variable string2.
+*/
+
+document.getElementById('frase').textContent =string2;
--- Unidad_2/00JavaScript/clase_condicional.js
+++ Unidad_2/00JavaScript/clase_condicional.js
@@ -2,5 +2,22 @@
  * Condicionales
  * Traer para mañana\
  * como se los utiliza
- * crear un html y mandar a llamar a llamar ese java
+ * crear un html y mandar a llamar a llamar ese java (cualquier cosa)
+ * @author bluebul
  */
+let valor = true;
+let condición1 = true;
+let condición2 = true;
+if (valor){
+    console.log("Se ejecutará esta sección porque se cumple la condición");
+}else {
+    console.log("Se ejecutará esta sección otro porque no se cumple la condición es falsa");
+}
+
+if (condition1) {
+  console.log("se ejecuta esta seccion cuando la condición es verdadera");
+} else if (condition2) {
+  console.log("código a ejecutar si la condición1 es falsa y la condición2 es verdadera");
+} else {
+  console.log("se ejecuta esta sección cuando la condición 1 y la condición2 son falsas");
+}
